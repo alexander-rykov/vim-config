902f18874b7fc152b2a46fc0c9e76fba9426ad56
Aliaksandr Rykau <aliaksandr_rykau@epam.com>
2020-06-28 15:04:40 +0300

    Move install-plugins.cmd. Add install-plugins.sh.

---
+++ pathogen/install-plugins.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+git clone https://tpope.io/vim/sensible.git ~/.vim-config/pathogen/bundle/vim-sensible
+git clone https://github.com/PProvost/vim-ps1.git ~/.vim-config/pathogen/bundle/vim-ps1
+git clone https://github.com/preservim/nerdtree.git ~/.vim-config/pathogen/bundle/nerdtree
+git clone https://github.com/itchyny/lightline.vim ~/.vim-config/pathogen/bundle/lightline.vim
